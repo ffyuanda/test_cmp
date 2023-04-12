d821c93fa19c585e77ef7e8613c75da5f5542b38
Shaoxuan Yuan <shaoxuan.yuan02@gmail.com>
2023-04-12 15:21:28 -0700

    small fix
    Signed-off-by: Shaoxuan Yuan <shaoxuan.yuan02@gmail.com>

--- test_cmp.sh
+++ test_cmp.sh
@@ -5,13 +5,12 @@
 # 'expected_output'
 # 
 # Steps:
-# 1. Create a t/ directory in the same level of your hw1/ directory
+# 1. Create a t/ directory in your hw1/ directory
 # 2. Put this test_cmp.sh file in the t/ directory
 # 3. Create a test file in the t/ directory (reference the test_example.sh)
 # 4. Source the test_cmp.sh file (reference line 3 from test_example.sh)
-# 4. Run the test file from your hw1/ directory (by doing ./t/test_example.sh)
+# 5. Run the test file from your hw1/ directory (by doing ./t/test_example.sh)
 test_cmp () {
-        mkdir -p t
         echo "Running ${1}" &&
         expect="t/${1}_expect" &&
         actual="t/${1}_actual" &&
